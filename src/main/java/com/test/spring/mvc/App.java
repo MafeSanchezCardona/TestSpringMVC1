91956a29e4c6e78f06482647569d85bd683c8aeb
msanchez <mafecharm@gmail.com>
2019-06-11 11:04:23 -0500

    Ajustes de test

--- src/main/java/com/test/spring/mvc/App.java
+++ src/main/java/com/test/spring/mvc/App.java
@@ -1,11 +1,21 @@
 package com.test.spring.mvc;
 
+import com.test.spring.mvc.manager.PersonManager;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//@PropertySource("classpath*:config.properties")
 public class App {
 
     public static void main(String[] args) {
+
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:applicationContext.xml");
+
+        PersonManager personManager = (PersonManager) context.getBean("personManager");
+
+        System.out.println("Init");
+
+        personManager.listPerson().forEach(item->System.out.println(item.getName()));
     }
 }
